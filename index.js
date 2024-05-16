2aa19569008da97af97504d405bc10ecc14b2845
bulbulahammedjibon <bulbulahammedgmail.com>
2024-05-16 11:23:28 +0600

    Hide mongodb key and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -7,10 +8,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
+ 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://travel-assigment:sjkm3yInNsYbvsYj@cluster0.fxm2qso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fxm2qso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
