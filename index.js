ef794c06c60bcfa36114c74c898d5cde6b184020
bulbulahammedjibon <bulbulahammedgmail.com>
2024-05-17 17:02:33 +0600

    final added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // const database = client.db("insertDB");
         // const haiku = database.collection("haiku");
 
@@ -94,7 +94,7 @@ async function run() {
             res.send(result);
         })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
